3f9d20f0a9aa884662432b724285b34a15d63bb4
erickcd57 <erick.cordova@unmsm.edu.pe>
2016-11-07 19:36:10 -0500

    Nuevo usuario
    Interfaz del nuevo usuario.

--- ProyectoVeterinaria/src/Interfaces/NuevoUsuario.java
+++ ProyectoVeterinaria/src/Interfaces/NuevoUsuario.java
@@ -27,18 +27,85 @@ public class NuevoUsuario extends javax.swing.JFrame {
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
-        setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+        lbRegistro = new javax.swing.JLabel();
+        lbNombre = new javax.swing.JLabel();
+        jTextField1 = new javax.swing.JTextField();
+        lbApPaterno = new javax.swing.JLabel();
+        jTextField2 = new javax.swing.JTextField();
+        lbApMaterno = new javax.swing.JLabel();
+        jTextField3 = new javax.swing.JTextField();
+        lbUsuario = new javax.swing.JLabel();
+        jTextField4 = new javax.swing.JTextField();
+        lbClave = new javax.swing.JLabel();
+        lbTipo = new javax.swing.JLabel();
+        jTextField5 = new javax.swing.JTextField();
+        jComboBox1 = new javax.swing.JComboBox<>();
+        jButton1 = new javax.swing.JButton();
+        jLabel1 = new javax.swing.JLabel();
 
-        javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
-        getContentPane().setLayout(layout);
-        layout.setHorizontalGroup(
-            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 400, Short.MAX_VALUE)
-        );
-        layout.setVerticalGroup(
-            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 300, Short.MAX_VALUE)
-        );
+        setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+        setMaximumSize(new java.awt.Dimension(550, 450));
+        setPreferredSize(new java.awt.Dimension(500, 440));
+        getContentPane().setLayout(null);
+
+        lbRegistro.setFont(new java.awt.Font("Comic Sans MS", 0, 11)); // NOI18N
+        lbRegistro.setText("REGISTRO NUEVO USUARIO:");
+        getContentPane().add(lbRegistro);
+        lbRegistro.setBounds(12, 12, 162, 17);
+
+        lbNombre.setFont(new java.awt.Font("Comic Sans MS", 0, 11)); // NOI18N
+        lbNombre.setText("NOMBRES:");
+        getContentPane().add(lbNombre);
+        lbNombre.setBounds(12, 48, 60, 17);
+        getContentPane().add(jTextField1);
+        jTextField1.setBounds(204, 47, 225, 20);
+
+        lbApPaterno.setFont(new java.awt.Font("Comic Sans MS", 0, 11)); // NOI18N
+        lbApPaterno.setText("APELLIDO PATERNO:");
+        getContentPane().add(lbApPaterno);
+        lbApPaterno.setBounds(12, 85, 115, 17);
+        getContentPane().add(jTextField2);
+        jTextField2.setBounds(204, 84, 225, 20);
+
+        lbApMaterno.setFont(new java.awt.Font("Comic Sans MS", 0, 11)); // NOI18N
+        lbApMaterno.setText("APELLIDO MATERNO:");
+        getContentPane().add(lbApMaterno);
+        lbApMaterno.setBounds(12, 122, 119, 17);
+        getContentPane().add(jTextField3);
+        jTextField3.setBounds(204, 121, 225, 20);
+
+        lbUsuario.setFont(new java.awt.Font("Comic Sans MS", 0, 11)); // NOI18N
+        lbUsuario.setText("USUARIO:");
+        getContentPane().add(lbUsuario);
+        lbUsuario.setBounds(12, 159, 57, 17);
+        getContentPane().add(jTextField4);
+        jTextField4.setBounds(204, 158, 225, 20);
+
+        lbClave.setFont(new java.awt.Font("Comic Sans MS", 0, 11)); // NOI18N
+        lbClave.setText("PASSWORD:");
+        getContentPane().add(lbClave);
+        lbClave.setBounds(12, 196, 69, 17);
+
+        lbTipo.setFont(new java.awt.Font("Comic Sans MS", 0, 11)); // NOI18N
+        lbTipo.setText("TIPO:");
+        getContentPane().add(lbTipo);
+        lbTipo.setBounds(12, 237, 31, 17);
+        getContentPane().add(jTextField5);
+        jTextField5.setBounds(204, 195, 225, 20);
+
+        jComboBox1.setFont(new java.awt.Font("Comic Sans MS", 0, 11)); // NOI18N
+        jComboBox1.setModel(new javax.swing.DefaultComboBoxModel<>(new String[] { "Veterinario(a)", "Secretaria(o)" }));
+        getContentPane().add(jComboBox1);
+        jComboBox1.setBounds(204, 232, 101, 26);
+
+        jButton1.setIcon(new javax.swing.ImageIcon(getClass().getResource("/Recursos/registro_opt.png"))); // NOI18N
+        jButton1.setText("REGISTRAR");
+        getContentPane().add(jButton1);
+        jButton1.setBounds(305, 309, 148, 55);
+
+        jLabel1.setIcon(new javax.swing.ImageIcon(getClass().getResource("/Recursos/Fondo.png"))); // NOI18N
+        getContentPane().add(jLabel1);
+        jLabel1.setBounds(0, -5, 500, 440);
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
@@ -79,5 +146,20 @@ public class NuevoUsuario extends javax.swing.JFrame {
     }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JButton jButton1;
+    private javax.swing.JComboBox<String> jComboBox1;
+    private javax.swing.JLabel jLabel1;
+    private javax.swing.JTextField jTextField1;
+    private javax.swing.JTextField jTextField2;
+    private javax.swing.JTextField jTextField3;
+    private javax.swing.JTextField jTextField4;
+    private javax.swing.JTextField jTextField5;
+    private javax.swing.JLabel lbApMaterno;
+    private javax.swing.JLabel lbApPaterno;
+    private javax.swing.JLabel lbClave;
+    private javax.swing.JLabel lbNombre;
+    private javax.swing.JLabel lbRegistro;
+    private javax.swing.JLabel lbTipo;
+    private javax.swing.JLabel lbUsuario;
     // End of variables declaration//GEN-END:variables
 }
