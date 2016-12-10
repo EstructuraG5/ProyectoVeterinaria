9c9263709b11e2fba9a3fa1a0bb077ddf2439da0
JuanSW18 <juan.eneque@unmsm.edu.pe>
2016-12-10 16:37:02 -0500

    Ordenar2
    Posible metodo

--- ProyectoVeterinaria/src/Clases/Odenar.java
+++ ProyectoVeterinaria/src/Clases/Odenar.java
@@ -8,6 +8,17 @@ import java.util.ArrayList;
  */
 public class Odenar {
     public void ordenarSeleccion(ArrayList data){
-        
+        int i, k, n;
+        String aux;
+        n = data.size();
+        for(i=1; i<n; i++){
+            //aux = data(i)
+            k = i - 1;
+            while(k >= 0){
+                // data(k+1) = data(k)
+                k = k - 1;
+            }
+            //data(k+1) = aux
+        }
     }
 }
