0bb6d63ffedaf0d4e8c39241bc5d93952dd15bb7
JuanSW18 <juan.eneque@unmsm.edu.pe>
2016-12-10 18:35:46 -0500

    hi

--- ProyectoVeterinaria/src/Clases/Ordenar.java
+++ ProyectoVeterinaria/src/Clases/Ordenar.java
@@ -8,14 +8,16 @@ import java.util.ArrayList;
  */
 public class Ordenar {
     
-    public void ordenarInsercion(ArrayList data){
+    public void ordenarInsercion(ArrayList<ResultadosBusqueda> data){
         int n, i, k;
         String aux;
+        ResultadosBusqueda resp = new ResultadosBusqueda();
         n = data.size();
         for(i=1; i<n; i++){
-            //aux = data(k)
+            aux = data.get(i).getCliente_apellidoMaterno();
             k = i - 1;
-            while(k >=0){ // && aux < data(k) uso de equals
+            while(k >=0 && aux.compareTo(data.get(k).getCliente_apellidoMaterno())<0){
+                
                 //data(k+1) = data(k)
                 k = k - 1;
             }
